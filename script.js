94763eb420253b8a39c4dcdc88ca3cb1ea4206f0
MCeciliaLuna <lunama.cecilia@live.com>
2023-05-24 15:47:58 -0300

    filtro de los mejores puntajes listo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("%cScores Final Mundial Qatar 2022 - Selección Argentina", "backgro
 
 const scores = [
   { player: "E. MARTÍNEZ", score: 10 },
-  { player: "N. MOLINA", score: 7 },
+  { player: "N. MOLINA", score: 5 },
   { player: "C. ROMERO", score: 9 },
   { player: "N. OTAMENDI", score: 9 },
   { player: "N. TAGLIAFICO", score: 8 },
@@ -12,13 +12,21 @@ const scores = [
   { player: "Á. DI MARÍA", score: 10 },
   { player: "L. MESSI", score: 10 },
   { player: "J. ÁLVAREZ", score: 8 },
-  { player: "M. ACUÑA", score: 7 },
+  { player: "M. ACUÑA", score: 6 },
   { player: "G. MONTIEL", score: 7 },
-  { player: "L. PAREDES", score: 7 },
+  { player: "L. PAREDES", score: 5 },
   { player: "L. MARTÍNEZ", score: 7 },
-  { player: "G. PEZZELLA", score: 7 },
-  { player: "P. DYBALA", score: 7 },
+  { player: "G. PEZZELLA", score: 6 },
+  { player: "P. DYBALA", score: 5 },
   { player: "L. SCALONI", score: 10 },
 ];
 
 console.table(scores)
+
+const outstandingScores = scores.filter(score => score.score > 7)
+
+const outstandingPlayers = outstandingScores.map(player => player.player)
+
+console.log("%cOutstanding Players & Scores", "background-color: #0096c7; color: white; font-size: 15px; text-align: center; padding:10px;margin: 5px");
+
+console.table(outstandingPlayers)

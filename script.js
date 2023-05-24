7f440f2a2add7d27ba118591eb7113c23fa3e2f2
MCeciliaLuna <lunama.cecilia@live.com>
2023-05-24 11:10:09 -0300

    creación y estilización de título en consola mas array de jugadores y scores

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("%cScores Final Mundial Qatar 2022 - Selección Argentina", "background-color: #0096c7; color: white; font-size: 20px; text-align: center ; padding: 10px ;  margin: 5px ");
+
+const scores = [
+  { player: "E. MARTÍNEZ", score: 10 },
+  { player: "N. MOLINA", score: 7 },
+  { player: "C. ROMERO", score: 9 },
+  { player: "N. OTAMENDI", score: 9 },
+  { player: "N. TAGLIAFICO", score: 8 },
+  { player: "R. DE PAUL", score: 7 },
+  { player: "E. FERNANDEZ", score: 9 },
+  { player: "A. MAC ALLISTER", score: 9 },
+  { player: "Á. DI MARÍA", score: 10 },
+  { player: "L. MESSI", score: 10 },
+  { player: "J. ÁLVAREZ", score: 8 },
+  { player: "M. ACUÑA", score: 7 },
+  { player: "G. MONTIEL", score: 7 },
+  { player: "L. PAREDES", score: 7 },
+  { player: "L. MARTÍNEZ", score: 7 },
+  { player: "G. PEZZELLA", score: 7 },
+  { player: "P. DYBALA", score: 7 },
+  { player: "L. SCALONI", score: 10 },
+];
+
+console.table(scores)

a5891cc03f55d8b0a168906f1ecb5d47282fde87
MCeciliaLuna <lunama.cecilia@live.com>
2023-05-25 00:13:34 -0300

    array modificado con eliminación de altos scores listo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-console.log("%cScores Final Mundial Qatar 2022 - Selección Argentina", "background-color: #0096c7; color: white; font-size: 20px; text-align: center ; padding: 10px ;  margin: 5px ");
+console.log(
+  "%cScores Final Mundial Qatar 2022 - Selección Argentina",
+  "background-color: #0096c7; color: white; font-size: 20px; text-align: center ; padding: 10px ;  margin: 5px "
+);
 
 const scores = [
   { player: "E. MARTÍNEZ", score: 10 },
@@ -21,12 +24,32 @@ const scores = [
   { player: "L. SCALONI", score: 10 },
 ];
 
-console.table(scores)
+console.table(scores);
 
-const outstandingScores = scores.filter(score => score.score > 7)
+const outstandingScores = scores.filter((score) => score.score > 7);
 
-const outstandingPlayers = outstandingScores.map(player => player.player)
+const outstandingPlayers = outstandingScores.map((player) => player.player);
 
-console.log("%cOutstanding Players & Scores", "background-color: #0096c7; color: white; font-size: 15px; text-align: center; padding:10px;margin: 5px");
+const players = outstandingPlayers;
 
-console.table(outstandingPlayers)
+console.log(
+  "%cOutstanding Players & Scores",
+  "background-color: #0096c7; color: white; font-size: 15px; text-align: center; padding:10px;margin: 5px"
+);
+
+console.log(players);
+
+console.log(
+  "%cRegular Players & Scores",
+  "background-color: #0096c7; color: white; font-size: 15px; text-align: center; padding:10px;margin: 5px"
+);
+
+for (let i = scores.length - 1; i >= 0; i--) {
+  if (outstandingScores.includes(scores[i])) {
+    scores.splice(i, 1);
+  }
+}
+
+const regularScores = scores;
+
+console.table(regularScores);

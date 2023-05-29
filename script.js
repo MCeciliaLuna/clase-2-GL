069f245adc1d54ea52d8dd19fc08f0b102031cf1
MCeciliaLuna <lunama.cecilia@live.com>
2023-05-29 16:39:47 -0300

    correcciones sustituyendo array de objetos por arrays nativos

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
-console.log(
-  "%cScores Final Mundial Qatar 2022 - Selección Argentina",
-  "background-color: #0096c7; color: white; font-size: 20px; text-align: center ; padding: 10px ;  margin: 5px "
-);
+const scores = [10, 5, 9, 9, 8, 7, 9, 9, 10, 10, 8, 6, 7, 5, 7, 6, 5, 10];
 
-const scores = [
-  { player: "E. MARTÍNEZ", score: 10 },
-  { player: "N. MOLINA", score: 5 },
-  { player: "C. ROMERO", score: 9 },
-  { player: "N. OTAMENDI", score: 9 },
-  { player: "N. TAGLIAFICO", score: 8 },
-  { player: "R. DE PAUL", score: 7 },
-  { player: "E. FERNANDEZ", score: 9 },
-  { player: "A. MAC ALLISTER", score: 9 },
-  { player: "Á. DI MARÍA", score: 10 },
-  { player: "L. MESSI", score: 10 },
-  { player: "J. ÁLVAREZ", score: 8 },
-  { player: "M. ACUÑA", score: 6 },
-  { player: "G. MONTIEL", score: 7 },
-  { player: "L. PAREDES", score: 5 },
-  { player: "L. MARTÍNEZ", score: 7 },
-  { player: "G. PEZZELLA", score: 6 },
-  { player: "P. DYBALA", score: 5 },
-  { player: "L. SCALONI", score: 10 },
+const players = [
+  "E. MARTÍNEZ",
+  "N. MOLINA",
+  "C. ROMERO",
+  "N. OTAMENDI",
+  "N. TAGLIAFICO",
+  "R. DE PAUL",
+  "E. FERNANDEZ",
+  "A. MAC ALLISTER",
+  "Á. DI MARÍA",
+  "L. MESSI",
+  "J. ÁLVAREZ",
+  "M. ACUÑA",
+  "G. MONTIEL",
+  "L. PAREDES",
+  "L. MARTÍNEZ",
+  "G. PEZZELLA",
+  "P. DYBALA",
+  "L. SCALONI",
 ];
 
-console.table(scores);
+console.log(`Array "SCORES": \n`, scores);
+console.log(`Array "PLAYERS": \n`, players);
 
-const outstandingScores = scores.filter((score) => score.score > 7);
+const evalPerformance = (scores, players) => {
+  const outstandingScores = scores.filter((score) => score > 7);
 
-const outstandingPlayers = outstandingScores.map((player) => player.player);
+  const outstandingPlayers = players.filter((_, index) => scores[index] > 7);
 
-const players = outstandingPlayers;
+  const regularScores = scores.filter((score) => score <= 7);
 
-console.log(
-  "%cOutstanding Players & Scores",
-  "background-color: #0096c7; color: white; font-size: 15px; text-align: center; padding:10px;margin: 5px"
-);
+  const regularPlayers = players.filter((_, index) => scores[index] <= 7);
 
-console.log(players);
+  console.log(`Mejores puntajes: \n`, outstandingScores);
+  console.log(`Mejores jugadores: \n`, outstandingPlayers);
+  console.log(`Puntajes regulares: \n`, regularScores);
+  console.log(`Jugadores regulares: \n`, regularPlayers);
+};
 
-console.log(
-  "%cRegular Players & Scores",
-  "background-color: #0096c7; color: white; font-size: 15px; text-align: center; padding:10px;margin: 5px"
-);
-
-for (let i = scores.length - 1; i >= 0; i--) {
-  if (outstandingScores.includes(scores[i])) {
-    scores.splice(i, 1);
-  }
-}
-
-const regularScores = scores;
-
-console.table(regularScores);
+evalPerformance(scores, players);
